ee47e87ba74855c45eeb33e42598e35e053066a9
ValievMarat <marat.valiev2014@yandex,ru>
2022-11-29 10:49:33 +0500

    dorabotki

--- advertisements/permissions.py
+++ advertisements/permissions.py
@@ -3,5 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['DELETE', 'PUT', 'PATCH']:
-            return request.user == obj.creator
+        return request.user == obj.creator
